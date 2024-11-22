fe25c973bbd464dc358d832f16bf1fdd626846a8
Ahlyab <ahlyabasad@gmail.com>
2024-11-22 23:36:25 +0500

    search implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
-import {client} from "@/sanity/lib/client";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
+import {sanityFetch, SanityLive} from "@/sanity/lib/live";
 
 export default async function  Home({searchParams}: {searchParams: Promise<{query?:string}>}) {
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const params = {search: query || null};
+  // const posts = await client.fetch(STARTUPS_QUERY);
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
+
+  console.log(posts);
 
     return (
     <>
@@ -26,6 +30,7 @@ export default async function  Home({searchParams}: {searchParams: Promise<{quer
           ) : ("No Startups")}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,16 +1,31 @@
 import {defineQuery} from "groq";
 
 export const STARTUPS_QUERY =
-    defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_CreatedAt desc) {
+    defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_CreatedAt desc) {
   _id,
     title,
     slug,
-    _CreatedAt,
+    _createdAt,
     author -> {
       _id, name, image, bio
     },
     views,
     description,
     category,
-    image
+    image,
+}`);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0] {
+  _id,
+    title,
+    slug,
+    _createdAt,
+    author -> {
+      _id, name, image, bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
 }`);
